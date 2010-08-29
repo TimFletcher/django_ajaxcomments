6042e749a388771f903b6cbc9eb1d9c7fa3bfdda
Tim Fletcher <tim@timothyfletcher.com>
2010-08-29 09:59:09 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This project is an unofficial fork of Brandon Konkle's DRY Ajax Comments -http://bitbucket.org/bkonkle/django-ajaxcomments/src/.
+
+I've converted the JS into a jQuery plugin to make it easier to use with varying markup. This isn't quite production ready. There's still a few kinks to iron out.
---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name = "django_ajaxcomments",
+    version = "0.4",
+    author = "Tim Fletcher",
+    author_email = "tim@timothyfletcher.com",
+    description = "Easily add AJAX-based comment functionality to a Django project.",
+    license = "BSD",
+    url = "http://github.com/TimFletcher/Django-Ajax-Comments",
+    packages = [
+        "django_ajaxcomments"
+    ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Programming Language :: JavaScript',
+    ]
+)
